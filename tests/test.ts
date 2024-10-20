773ee67e6f6686ad5cec7de18d8b9b865d5b79d9
Michael Mills <mmills6060@gmail.com>
2024-10-19 23:00:17 -0400

    built out backend, added tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,4 +1,9 @@
+import { neuranet } from '../src/renderer/neuranet';
+import { test_device_info } from '../src/renderer/neuranet/device/test_device_info';
+
 test('hello world', () => {
   expect('hello world').toBe('hello world')
 });
+
+test_device_info();
 
